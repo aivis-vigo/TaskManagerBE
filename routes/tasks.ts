e21d72719664e1d365fd2d3336b5cb1390067012
aivis-vigo <aivisvigoreimarts@gmail.com>
2024-10-31 09:00:37 +0200

    updated connection string

--- routes/tasks.ts
+++ routes/tasks.ts
@@ -1,7 +1,7 @@
 import express, {Request, Response, NextFunction} from "express";
 import mongoose, {Schema} from "mongoose";
 
-mongoose.connect(`${process.env.ATLAS_URI as string}/angular-todo`)
+mongoose.connect(process.env.ATLAS_URI as string)
     .then(() => console.log('Connected to MongoDB'))
     .catch((err) => console.error('Failed to connect to MongoDB', err));
 
